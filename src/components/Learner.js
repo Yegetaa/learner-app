d68e8be549a844ddca39414ae9c523bd2a2fa7e5
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-01-26 13:35:00 -0500

    Learner Component Created

---
+++ src/components/Learner.js
@@ -0,0 +1,11 @@
+
+
+export function Learner({ name, bio }) {
+    return (
+      <div>
+        <h2>Name: {name}</h2>
+        <p>Bio: {bio}</p>
+        {/* <Score></Score> */}
+      </div>
+    );
+  }
